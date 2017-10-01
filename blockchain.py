0ffc55897f8415ecd89e33947b3b8c692d797f7e
Ian Kirkpatrick <kirkpatrickian6@gmail.com>
2017-09-30 22:53:44 -0400

    added proof_of_work function, and hash, valid_proof methods

--- blockchain.py
+++ blockchain.py
@@ -34,7 +34,7 @@ class Blockchain(object):
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
-        #reset the current list of transactions
+        # reset the current list of transactions
         self.current_transactions = []
 
         self.chain.append(block)
@@ -59,12 +59,50 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        # hashes a block
-        pass
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work algorithm (PoW):
+        - find a number p' such that has(pp') contains leading 4 0's where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
 
     @property
     def last_block(self):
-        # returns last block in the chain
-        pass
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a block
+
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        #The dictionary must be ordered or there will be inconsistent hashes
+        block_string =  json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        validates the proof: does hash(last_proof, proof) checks for containing 4 leading 0's
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct proof, False if not
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
